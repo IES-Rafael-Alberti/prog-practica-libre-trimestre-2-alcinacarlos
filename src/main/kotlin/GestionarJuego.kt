89aa6d526362ddd6679e32e4b3cf1b0aec2e0be9
alcinacarlos <calcrom0607@g.educaand.es>
2024-03-07 21:11:32 +0100

    creando las primeras clases

--- src/main/kotlin/GestionarJuego.kt
+++ src/main/kotlin/GestionarJuego.kt
@@ -5,7 +5,9 @@ object GestionarJuego {
         println("Bienvenido a Mining Simulator Ultimate Deluxe Premium Edition (nombre muy original)")
         println("En este juego, comienzas siendo el dueño de una MINA, tu objetivo es gestionar la mina de la manera más eficiente sin llegar a bancarota")
         println("Cada 3 dias recibes un pedido que tienes que cumplir en un plazo, si no lo cumples a tiempo te resta dinero")
-        println("Cada 5 dias los trabajadores cobran su sueldo y tienes que pagar el alquiler del local y maquinaria")
-        println("Con esto es suficiente, lo demás lo verás sobre la marcha, mucha suerte y cuidado con los gases!")
+        println("Cada 5 dias los trabajadores cobran su sueldo")
+        println("Cada 10 dias tienes que pagar el alquiler de maquinaria")
+        println("Si te quedas en bancarota pierdes")
+        println("Con esta explicacion es suficiente, lo demás lo verás sobre la marcha, mucha suerte y cuidado con los gases!")
     }
 }
