f803736c3d3678859369a763062081022588bad5
alcinacarlos <calcrom0607@g.educaand.es>
2024-03-12 19:51:48 +0100

    cookeando

--- src/main/kotlin/GestionarJuego.kt
+++ src/main/kotlin/GestionarJuego.kt
@@ -1,6 +1,13 @@
 package org.practicatrim2
+import com.github.ajalt.mordant.rendering.TextColors.*
+import com.github.ajalt.mordant.rendering.TextStyles.*
+import com.github.ajalt.mordant.terminal.Terminal
+
+import kotlin.math.min
 
 object GestionarJuego {
+    val t = Terminal()
+
     fun presentarJuego(){
         println("Bienvenido a Mining Simulator Ultimate Deluxe Premium Edition (nombre muy original)")
         println("En este juego, comienzas siendo el dueño de una MINA, tu objetivo es gestionar la mina de la manera más eficiente sin llegar a bancarota")
@@ -9,5 +16,19 @@ object GestionarJuego {
         println("Cada 10 dias tienes que pagar el alquiler de maquinaria")
         println("Si te quedas en bancarota pierdes")
         println("Con esta explicacion es suficiente, lo demás lo verás sobre la marcha, mucha suerte y cuidado con los gases!")
+    }
+    fun iniciarDia(mina:Mina){
+        mina.avanzarDia()
+        println("""
+            Dia: ${mina.dia}
+            Has llegado 
+        """)
+
+    }
+    fun iniciarMina(mina:Mina){
+        do {
+            iniciarDia(mina)
+
+        }while (mina.activa)
     }
 }
--- src/main/kotlin/Mina.kt
+++ src/main/kotlin/Mina.kt
@@ -3,9 +3,30 @@ package org.practicatrim2
 class Mina(
     val nombre:String,
     var listaTrabajadores:MutableList<Trabajador>,
-    var historialAcciones: MutableMap<String, MutableList<String>> = mutableMapOf(),
-    val estadoMina:Boolean = false, //Indica si la mina está terminada o no
-    val dia: Int = 0,
+    var historialAcciones: MutableMap<Int, MutableList<String>> = mutableMapOf(),
+    var activa:Boolean = true, //Indica si la mina está terminada o no
+    var dia: Int = 0,
     val pedidosRealizados:MutableList<Pedido> = mutableListOf()
 ) {
+    fun avanzarDia(){
+        dia++
+    }
+    fun trabajar(){
+        listaTrabajadores.forEach { it.trabajar() }
+    }
+
+    override fun toString(): String {
+        return """
+        Mina: ${nombre}
+        Activa: ${activa}
+        Día: ${dia}
+        Trabajadores: ${listaTrabajadores.size}
+        Pedidos realizados: ${pedidosRealizados.size}
+        Historial de acciones:
+        ${historialAcciones.map { (dia, acciones) ->
+            "$dia: ${acciones.joinToString(", ")}"
+        }.joinToString("\n")}
+    """
+    }
+
 }
